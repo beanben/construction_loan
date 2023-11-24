a681ecace87a7e43327175b0879c58dd1a2733e2
Ben <base_kh@hotmail.com>
2023-11-24 10:54:07 +0000

    notebook with cool graphs - committing before sharing

--- construction_loan/cashflow.py
+++ construction_loan/cashflow.py
@@ -2,7 +2,7 @@ import pandas as pd
 from construction_loan.utils import time_execution, dataframe_to_csv
 
 class Cashflow:
-    billing_period = 'end of calendar month'
+    billing_period = 'M'
 
     def __init__(self, data):
             if isinstance(data, pd.DataFrame):
@@ -15,7 +15,7 @@ class Cashflow:
     def from_budget(cls, budget_instance):
 
         # get the budget dataframe
-        budget_df = budget_instance.df
+        budget_df = budget_instance.df.copy()
 
         # Calculate the number of days and daily cost
         budget_df['num_days'] = (pd.to_datetime(budget_df['end_date']) - pd.to_datetime(budget_df['start_date'])).dt.days + 1
@@ -41,23 +41,47 @@ class Cashflow:
             daily_costs.iloc[-1] += row['amount'] - daily_costs.sum()
             cashflow_df.loc[daily_costs.index, (row['cost_category'], row['cost_type'], row['supplier'])] += daily_costs.values
 
-        return cls(cashflow_df)
+        
+        # group the cashflow by billing period
+        cashflow_df = cashflow_df.resample(cls.billing_period).sum()
 
-    # @classmethod
-    # def from_csv(cls, csv_file_path):
-    #     # Logic to read cash flow data directly from CSV
-    #     cashflow_df = ...  # Read and process CSV file to create cash flow DataFrame
-    #     return cls(cashflow_df)
+        return cls(cashflow_df)
 
     def total_cost(self):
         return self.df.sum().sum()
-    
-    def costs_grouped_by_billing_period(self):
-        billing_period = 'M' if self.billing_period == 'end of calendar month' else 'D'
-        return self.df.resample(billing_period).sum()
-    
+
     def costs_grouped_by_level(self, level):
-        return self.df.sum(level=level, axis=1)
+        df = self.df.T.groupby(level=level).sum().T
+        df['total'] = df.sum(axis=1)
+        return df
+    
+    # summarise the cashflow by key cost_category: acquisition costs, construction costs and put the rest in a new cateogry called other costs
+    def costs_summary(self):
+        # Group the cashflow by cost_category and sum
+        grouped_df = self.df.T.groupby(level='cost_category').sum().T
+
+        # Calculate the total for each cost_category
+        total_by_category = grouped_df.sum(axis=0)
+
+        # Identify the top 4 categories
+        top4_categories = total_by_category.nlargest(4).index
+
+       # Create a new DataFrame to store the summarized data
+        cashflow_df_summary = pd.DataFrame()
+
+        # Add the top 4 categories as they are
+        for category in top4_categories:
+            cashflow_df_summary[category] = grouped_df[category]
+
+        # group from the grouped_df the rest of the categories in a new category called other
+        other_data = grouped_df.loc[:, ~grouped_df.columns.isin(top4_categories)].copy()
+        cashflow_df_summary['other'] = other_data.sum(axis=1)
+        
+        # add a total column
+        cashflow_df_summary['total'] = cashflow_df_summary.sum(axis=1)
+
+        return cashflow_df_summary
+
     
     def df_to_csv(self, csv_file_path):
         dataframe_to_csv(self.df, csv_file_path)
--- construction_loan/visualiser.py
+++ construction_loan/visualiser.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import plotly.graph_objs as go
+from construction_loan.cashflow import Cashflow
 
 class DataVisualiser:
     def __init__(self):
@@ -11,8 +13,88 @@ class DataVisualiser:
     def display_dataframe(self, df):
         return df
 
-    @staticmethod
-    def plot_graph(data):
-        # Implement graph plotting logic
-        plt.plot(data)
-        plt.show()
+    def plot_cost_summary(self, cashflow_instance):
+        # Check if the provided object is an instance of Cashflow
+        if not isinstance(cashflow_instance, Cashflow):
+            raise TypeError("The provided object is not an instance of Cashflow")
+
+        # Flatten the MultiIndex for easier handling
+        df = cashflow_instance.df.copy()
+        df.columns = [' '.join(map(str, col)).strip() for col in df.columns.values]
+
+        # Create a line plot for each cost_type within each cost_category
+        fig = go.Figure()
+        for column in df.columns:
+            cost_category, cost_type = column.split(' ', 1)
+            fig.add_trace(go.Scatter(
+                x=df.index, 
+                y=df[column], 
+                mode='lines+markers', 
+                name=column, 
+                hoverinfo='text',
+                text=[f"{column}: {y}" for y in df[column]],
+                line=dict(dash='solid' if cost_category == 'Acquisition costs' else 'dot')
+            ))
+
+        # Update layout for the graph
+        fig.update_layout(
+            title='Total Costs by Category and Period',
+            xaxis_title='Period',
+            yaxis_title='Total Amount',
+            legend=dict(
+                orientation="v",  # Vertical orientation
+                yanchor="middle",
+                y=0.5,
+                xanchor="left",
+                x=1.05  # Position the legend to the right of the plot
+            ),
+            autosize=True,
+            width=1200,  # Adjust width to accommodate the legend
+            height=600,
+            margin=dict(l=10, r=10, t=40, b=40)  # Adjust right margin for legend
+        )
+
+        fig.show()
+
+    def plot_cumulative_cost_summary(self, cashflow_instance):
+        # Check if the provided object is an instance of Cashflow
+        if not isinstance(cashflow_instance, Cashflow):
+            raise TypeError("The provided object is not an instance of Cashflow")
+
+        # Flatten the MultiIndex for easier handling
+        df = cashflow_instance.df.copy()
+        df.columns = [' '.join(map(str, col)).strip() for col in df.columns.values]
+
+        # Calculate cumulative sum for each column
+        cumulative_df = df.cumsum()
+
+        # Create a stacked bar chart for each cost_type within each cost_category
+        fig = go.Figure()
+        for column in cumulative_df.columns:
+            fig.add_trace(go.Bar(
+                x=cumulative_df.index, 
+                y=cumulative_df[column], 
+                name=column,
+                # text=[f"{column}: {y}" for y in cumulative_df[column]]
+            ))
+
+        # Update layout for the graph
+        fig.update_layout(
+            title='Cumulative Costs by Category and Period',
+            xaxis_title='Period',
+            yaxis_title='Cumulative Amount',
+            barmode='stack',  # Stack bars for different cost types
+            legend=dict(
+                orientation="v",
+                yanchor="middle",
+                y=0.5,
+                xanchor="left",
+                x=1.05
+            ),
+            autosize=True,
+            width=1200,
+            height=600,
+            margin=dict(l=10, r=10, t=40, b=40)
+        )
+
+        fig.show()
